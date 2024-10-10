346eb9d414528a4b61a6242c776ae493e475aa91
Javier Alfaro <ojalfarob@gmail.com>
2024-10-10 08:10:15 -0600

    matches routes

---
+++ Api/controllers/matchController.js
@@ -0,0 +1,110 @@
+import User from "../models/userModel.js";
+
+export const swipeRight = async (req, res) => {
+  try {
+    const { likedUserId } = req.params;
+    const currentUser = User.findById(req.user.id);
+    const likeUser = await User.findById(likedUserId);
+
+    if (!likeUser) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (!currentUser.likes.includes(likedUserId)) {
+      currentUser.likes.push(likedUserId);
+      await currentUser.save();
+
+      if (likeUser.likes.includes(currentUser.id)) {
+        currentUser.matches.push(likedUserId);
+        likeUser.matches.push(currentUser.id);
+        await Promise.all([await currentUser.save(), await likeUser.save()]);
+      }
+    }
+    res.status(200).json({
+      success: true,
+      user: currentUser,
+    });
+  } catch (error) {
+    console.log("Error in swipeRight: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+export const swipeLeft = async (req, res) => {
+  try {
+    const { dislikedUserId } = req.params;
+    const currentUser = User.findById(req.user.id);
+
+    if (!currentUser.dislikes.includes(dislikedUserId)) {
+      currentUser.dislikes.push(dislikedUserId);
+      await currentUser.save();
+    }
+    res.status(200).json({
+      success: true,
+      user: currentUser,
+    });
+  } catch (error) {
+    console.log("Error in swipeLeft: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getMatches = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).populate(
+      "matches",
+      "name image"
+    );
+
+    res.status(200).json({
+      success: true,
+      matches: user.matches,
+    });
+  } catch (error) {
+    console.log("Error in getMatches: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getUserProfiles = async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.user.id);
+
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: currentUser.id } },
+        { _id: { $nin: currentUser.likes } },
+        { _id: { $nin: currentUser.dislikes } },
+        { _id: { $nin: currentUser.matches } },
+        {
+          gender:
+            currentUser.genderPreference === "both"
+              ? { $in: ["male", "female"] }
+              : currentUser.genderPreference,
+        },
+        { genderPreference: { $in: [currentUser.gender, "both"] } },
+      ],
+    });
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    console.log("Error in getUserProfiles: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- Api/routes/matchesRoutes.js
+++ Api/routes/matchesRoutes.js
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.js";
+import {
+  swipeRight,
+  swipeLeft,
+  getMatches,
+  getUserProfiles,
+} from "../controllers/matchController.js";
 
-const router = Router()
+const router = Router();
 
+router.post("/swipe-right/:likedUserId", protectRoute, swipeRight);
+router.post("/swipe-left/:dislikedUserId", protectRoute, swipeLeft);
 
-export default router
+router.get("", protectRoute, getMatches);
+router.get("/user-profiles", protectRoute, getUserProfiles);
+
+export default router;
