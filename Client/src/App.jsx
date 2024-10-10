bb6cf8cb407419d09956bfac97be8015fb56b858
Javier Alfaro <ojalfarob@gmail.com>
2024-10-10 09:17:38 -0600

    reacts setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
+import ChatPage from "./pages/ChatPage";
+
+const App = () => {
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
